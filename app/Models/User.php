773a4ac576dc35ad083b9a9017f9d6befc32cc3e
Mirko Lorusso <mirko.lorusso@gmail.com>
2025-11-27 16:42:42 +0100

    Add getters and setters to User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -45,4 +45,40 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function getId(): int|null
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id): User
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    public function getName(): string|null
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): User
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getEmail(): string|null
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): User
+    {
+        $this->email = $email;
+
+        return $this;
+    }
 }
